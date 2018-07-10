845d14d8e7ae80cc341091b6b8761707cfa2c99f
Genevieve Masioni <masionigenevieve@gmail.com>
2018-07-10 15:52:15 +0200

    about section css

--- IKAE/assets/scripts/main.js
+++ IKAE/assets/scripts/main.js
@@ -2,5 +2,4 @@ window.onload = function() {
   // Cover section (home)
   var homeCover = document.querySelector(".cover");
   homeCover.style.height = window.innerHeight + "px";
-  console.log(homeCover.style);
 }
