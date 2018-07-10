8fee156b9cf9f5a3f3dc72d32b17c4bbcbc2cc27
Genevieve Masioni <masionigenevieve@gmail.com>
2018-07-10 14:03:00 +0200

    Cover component sass + js

---
+++ IKAE/assets/scripts/main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  // Cover section (home)
+  var homeCover = document.querySelector(".cover");
+  homeCover.style.height = window.innerHeight + "px";
+  console.log(homeCover.style);
+}
